c30b530ded2da0e6cd0ef41f386ea03b612b714d
Bjarke Brodin <Bjarke@brodin.io>
2022-06-19 18:25:00 +0200

    Update formula

--- Formula/upload.rb
+++ Formula/upload.rb
@@ -1,14 +1,11 @@
 class Upload < Formula
   desc "a small simple scp based file upload utility"
   homepage "https://github.com/bjarkebrodin/upload"
-  url "https://github.com/bjarkebrodin/upload/archive/refs/tags/v0.0.0.tar.gz"
-  sha256 "39e215c8b64daeabf325945ff510293ed7f7b3c2d5d6c0b5cc0c6f14c5ab45b2"
+  url "https://github.com/bjarkebrodin/upload/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "942b98b26bd5082cc2c5561c1e97fe9546fcb66206b72ae6cef755123ef20fcb"
   license "MIT"
 
-  depends_on "scp"
-
   def install
-    system "make"
     system "make", "PREFIX=#{prefix}", "install"
   end
 
