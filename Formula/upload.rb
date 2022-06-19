16232caff4a47becb9a33717ebac708c9b6fb395
Bjarke Brodin <Bjarke@brodin.io>
2022-06-19 18:09:16 +0200

    Update formula

--- Formula/upload.rb
+++ Formula/upload.rb
@@ -5,9 +5,14 @@ class Upload < Formula
   sha256 "39e215c8b64daeabf325945ff510293ed7f7b3c2d5d6c0b5cc0c6f14c5ab45b2"
   license "MIT"
 
+  depends_on "scp"
+
   def install
+    system "make"
+    system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
+    assert_match "test", shell_output("#{bin}/upload test")
   end
 end
