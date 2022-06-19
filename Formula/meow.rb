ae7b30c7b4dcb58b72d34008a1aaf7de3605218e
Bjarke Brodin <Bjarke@brodin.io>
2022-06-19 20:47:40 +0200

    add `meow`

---
+++ Formula/meow.rb
@@ -0,0 +1,17 @@
+class Meow < Formula
+  desc "A submission/utility client for the kattis online judging system"
+  homepage "https://github.com/bjarkebrodin/meow"
+  url "https://github.com/bjarkebrodin/meow/archive/refs/tags/v0.0.0.tar.gz"
+  sha256 "ca7e61eb11164985b23d9a5454a04e2171551742338e6c2536ff898bf024f903"
+  license "MIT"
+
+  def install
+    system "mkdir", "#{prefix}/bin/"
+    system "install", "-m", "+rx", "./*.py", "#{prefix}/bin/"
+    system "install", "-m", "+rx", "./meow", "#{prefix}/bin/"
+  end
+
+  test do
+    assert_match "test", shell_output("#{bin}/meow test")
+  end
+end
