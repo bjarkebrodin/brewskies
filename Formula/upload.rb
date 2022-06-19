4c6687232f64eaa42a6977336b3d986256dc4b22
Bjarke Brodin <Bjarke@brodin.io>
2022-06-19 17:05:30 +0200

    Update formula

--- Formula/upload.rb
+++ Formula/upload.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Upload < Formula
   desc "a small simple scp based file upload utility"
   homepage "https://github.com/bjarkebrodin/upload"
@@ -8,28 +5,9 @@ class Upload < Formula
   sha256 "39e215c8b64daeabf325945ff510293ed7f7b3c2d5d6c0b5cc0c6f14c5ab45b2"
   license "MIT"
 
-  # depends_on "cmake" => :build
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    echo "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test upload`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    # system "false"
-    echo "test"
   end
 end
